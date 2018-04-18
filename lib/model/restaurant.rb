4f0d37f29ddd65ec09ed12eb392da10fa9d54c85
SuperJesseH <therealjhorwitz@gmail.com>
2018-04-18 17:34:26 -0400

    got most emotion methods working

--- lib/model/restaurant.rb
+++ lib/model/restaurant.rb
@@ -3,14 +3,23 @@ class Restaurant < ActiveRecord::Base
   has_many :users, through: :reviews
 
   @@emos = ["angry", "sarcasm", "happy", "excited", "sad", "bored", "fear"]
-  @@break = "/////////////////////BREAK/////////////////////"
+  @@break = "/////////////////////END OF RESULTS/////////////////////"
 
   #List all resturants/users by name
   def  self.all_names
     all.map {|restaurant| restaurant.name}
   end
 
+  def self.happy_list
+    a = all.sort_by {|restaurant| restaurant.emotions_dirty[2]}.reverse!
+    a.each do |restaurant|
+      puts "#{restaurant.name}: #{make_pretty_number(restaurant.emotions_dirty[2])}%"
+    end
+    @@break
+  end
+
   def emotions_dirty
+    #consider sorting by value make sure lables match values (see #emotions)
     @@emos.map do |emotion|
       self.reviews.average(emotion.to_sym)
     end
@@ -18,11 +27,19 @@ class Restaurant < ActiveRecord::Base
 
   def emotions
     emotions_dirty.each_with_index do |emotion, index|
-      value = emotion.to_f
-      value = (value*100).round(2)
-      puts "#{@@emos[index].capitalize}: #{value}%"
+      puts "#{@@emos[index].capitalize}: #{make_pretty_number(emotion.to_f)}%"
     end
     @@break
+  end
+
+  def self.make_pretty_number(nasty_num)
+    n = nasty_num.to_f
+    (n*100).round(2)
+  end
+
+  def make_pretty_number(nasty_num)
+    n = nasty_num.to_f
+    (n*100).round(2)
   end
 
 end
--- lib/model/user.rb
+++ lib/model/user.rb
@@ -2,39 +2,44 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :restaurants, through: :reviews
 
+  @@emos = ["angry", "sarcasm", "happy", "excited", "sad", "bored", "fear"]
+  @@break = "/////////////////////END OF RESULTS/////////////////////"
 
-  def self.all_names
+  #List all resturants/users by name
+  def  self.all_names
     all.map {|user| user.name}
   end
 
+  def self.happy_list
+    a = all.sort_by {|user| user.emotions_dirty[2]}.reverse!
+    a.each do |user|
+      puts "#{user.name}: #{make_pretty_number(user.emotions_dirty[2])}%"
+    end
+    @@break
+  end
 
-  def emotions
-    emos = ["angry", "sarcasm", "happy", "excited", "sad", "bored", "fear"]
-    emos.map do |emotion|
+  def emotions_dirty
+    #consider sorting by value make sure lables match values (see #emotions)
+    @@emos.map do |emotion|
       self.reviews.average(emotion.to_sym)
     end
   end
 
+  def emotions
+    emotions_dirty.each_with_index do |emotion, index|
+      puts "#{@@emos[index].capitalize}: #{make_pretty_number(emotion.to_f)}%"
+    end
+    @@break
+  end
 
+  def self.make_pretty_number(nasty_num)
+    n = nasty_num.to_f
+    (n*100).round(2)
+  end
 
-
-
-
-  # def emotions_by_review_ugly
-  #   # emos = ["angry", "sarcasm", "happy", "excited", "sad", "bored", "fear"]
-  #   self.reviews.map do |review|
-  #     {angry: review.angry, sarcasm: review.sarcasm, happy: review.happy, excited: review.excited, sad: review.sad, bored: review.bored, fear: review.fear}
-  #   end
-  # end
-  #
-  # def emotions
-  #   angry = 0
-  #   sarcasm = 0
-  #   emotions_by_review_ugly.map do |review|
-  #     review.map do |emotion, emotion_value|
-  #       emotion
-  #     end
-  #   end
-  # end
+  def make_pretty_number(nasty_num)
+    n = nasty_num.to_f
+    (n*100).round(2)
+  end
 
 end
